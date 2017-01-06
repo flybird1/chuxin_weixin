01dd942d4c31b32d366647717108f4f81cbc84ce
liqiaomei <1242371759@qq.com>
2017-01-06 18:58:30 +0800

    add sth

---
+++ src/views/SkillDetal.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- root element is required， and the class must be **-view -->
+  <div class=""> 
+  
+  </div>
+</template>
+
+<script>
+  // import public component
+
+  // your vue code based on es6 syntax
+  export default {
+    components: {
+    }
+  }
+</script>
+
+<style lang="scss">
+  /* import public class */
+ 
+
+</style>
